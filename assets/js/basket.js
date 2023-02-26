52e936dd88a5efb82ff23e895782875a58f21bbc
unknown <royaam@code.edu.az>
2023-02-27 01:45:29 +0400

    end

--- assets/js/basket.js
+++ assets/js/basket.js
@@ -1,11 +1,6 @@
 "use strict";
 
 let tableBody = document.querySelector("tbody");
-
-
-
-
-
 let products = JSON.parse(localStorage.getItem("basket"));   //localStoragden datalari goturuk
 if (products != null) {       //eyer storagede data varsa ele yoxdusa yox
     for (const product of products) {                //hemin datalari saliriq arraye ve onlarin sayi geder table tagine -td elave edirik
@@ -52,7 +47,6 @@ let deleteIcons = document.querySelectorAll("tbody tr td i");
 for (const icon of deleteIcons) {
 
     icon.addEventListener("click", function () {
-        console.log(icon.nextElementSibling)
 
         let temp = products.filter(m => m.id != icon.parentNode.nextElementSibling.innerText); //yeni array yaradiram  hansi silmek istediyim data yoxdu icinde ve  silmek istediyim elementde delete-iconla siblinqle catiram
         products = temp;                 //sonra storagedeki arrayi essayn edirem yeni arraye hansindaki o data yoxdur
@@ -74,15 +68,11 @@ for (const icon of deleteIcons) {
 
 
 
-
 function totalPrice(arr) {     //function-komeyi ile butun mehsullarin toplam giymetin tapiriq
     let sum = 0;
     for (const item of arr) {
         sum += parseInt(item.price) * parseInt(item.count)   //parse edib localdaki datanin qiymetini,vururuq o qiymeti hemin mehsulusun sayina ki sayina uyqun qiymet elde edek
     }
-
-
-
     document.querySelector(".total h3").innerHTML += `<span>${sum} AU$</span>`   //ve butun umumi toplami yazdiriq var olan h -tagine
 
 }
@@ -92,7 +82,7 @@ let minusIcons = document.querySelectorAll("tbody tr td .minus");
 
 
 for (const icon of minusIcons) {           //minus icona basanda gedib mehsulun hem UI-daki sayini hemde local storagdaki sayini azaldiriq hemin mehsulun
-
+    
     icon.addEventListener("click", function () {    //minus icona basanda
         for (const product of products) {          //localstoragedaki datalari fora salaq
 
@@ -103,12 +93,13 @@ for (const icon of minusIcons) {           //minus icona basanda gedib mehsulun 
                 }
 
                 icon.nextElementSibling.innerText--;                   //UI-daki sayi azaldaq
-                product.count--;                                       //ve localstoragdeka hemin productun sayini azaldaq
-            }
+                product.count--;                                       //hemin productun localstoragede sayini azalt
+                totalPrice(products)            //ve cem qiymeti hesablayan functiona yeni array gonderey
+                window.location.reload();        //hazir refres etmek ucun Totalda kohne qiymet qalmasin deye                                      //ve localstoragdeka hemin productun sayini azaldaq
+            } 
 
         } 
-        totalPrice(products)            //ve cem qiymeti hesablayan functiona yeni array gonderey
-        window.location.reload();        //hazir refres etmek ucun Totalda kohne qiymet qalmasin deye
+        
         localStorage.setItem("basket", JSON.stringify(products))   //localstorage yenilenmis array yerlesdirek
     })
 
@@ -121,17 +112,18 @@ let plusIcons = document.querySelectorAll("tbody tr td .plus"); //plus icon basa
 for (const icon of plusIcons) {
 
     icon.addEventListener("click", function () {
-
+       
         for (const product of products) {
-
+               
             if (product.id == icon.parentNode.nextElementSibling.nextElementSibling.innerText) {    //hemin plus icona uyqun productu idisine gore tapaq
                 icon.previousElementSibling.innerText++;
                 product.count++;
+                totalPrice(products)             //ve cem qiymeti hesablayan functiona yeni array gonderey
+                window.location.reload();        //hazir refres etmek ucun Totalda kohne qiymet qalmasin deye
             }
 
         }
-        totalPrice(products)             //ve cem qiymeti hesablayan functiona yeni array gonderey
-        window.location.reload();        //hazir refres etmek ucun Totalda kohne qiymet qalmasin deye
+        
         localStorage.setItem("basket", JSON.stringify(products))   //localstorage yenilenmis array yerlesdirek
     })
 
